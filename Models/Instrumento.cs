72de7cd33b6bc18147a157e7060d1bf60c7c8d21
AndreaArely <andreaarelycalderongonzalez@gmail.com>
2026-07-22 18:07:42 -0600

    Desarrollo de models

---
+++ Models/Instrumento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Instrumentos.Models
+{
+    public class Instrumento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public string Marca {  get; set; }
+        public string Provedor { get; set; }
+        public float precio_compra {  get; set; }
+        public float precio_venta { get; set; }
+        public int stock {  get; set; }
+        public int stock_minimo { get; set; }
+        public string descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
